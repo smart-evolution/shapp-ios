3eddc8ff2dab596e00727ca8e5e5a8d08a2c23e9
Oskar Szura <oskar.szura@gmail.com>
2021-01-12 11:29:19 +0100

    Initial Commit

---
+++ smarthome/smarthomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  smarthomeApp.swift
+//  smarthome
+//
+//  Created by Oskar on 12/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct smarthomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
