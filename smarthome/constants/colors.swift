b00ac3384977d18a5cd81a9382abd5d356ecdac8
Oskar Szura <oskar.szura@gmail.com>
2021-01-15 15:37:03 +0100

    HOME-321 - Login page layouting

---
+++ smarthome/constants/colors.swift
@@ -0,0 +1,14 @@
+//
+//  constants.swift
+//  smarthome
+//
+//  Created by Oskar on 15/01/2021.
+//
+
+import SwiftUI
+import UIKit
+
+struct CColors {
+    static let blue = Color(UIColor.systemBlue)
+    static let gray = Color(UIColor(red: 200/255, green: 200/255, blue: 200/255, alpha: 1))
+}
---
+++ smarthome/constants/sizings.swift
@@ -0,0 +1,11 @@
+//
+//  sizings.swift
+//  smarthome
+//
+//  Created by Oskar on 15/01/2021.
+//
+import SwiftUI
+
+struct CSizings {
+    static let rounding: CGFloat = 6.0
+}
